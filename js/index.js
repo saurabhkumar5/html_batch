483d8048c86d0e997093830ac274e0fa9af7fbcf
saurabh kumar <saurabhkumar9493@gmail.com>
2023-12-27 19:55:12 +0530

    js

---
+++ js/index.js
@@ -0,0 +1,211 @@
+// console.log("sonu"); 
+// console.log(12); 
+// console.log("mohali")
+// console.log(4+4);
+
+// keywords let, const, var
+
+// let a = 10;
+// let b = 20;
+// let saurabh = 50;
+
+// console.log(a+b);
+// console.log(saurabh)
+
+// types of datatype
+
+// 1. Number,
+// 2. boolean
+// 3. string
+// 4. undefined
+// 5. null
+// 6. bigint
+// 7. symbol
+
+// number 1, 1.0 , 0.2, -1, 
+
+//string a, amit, A, 
+// let a = 1;
+// let b = 'amit'
+// let c = amit;
+// let a = "amit"
+// let f = 'a'
+
+// let a = 100;
+// let b = 20;
+
+// // a>b
+// // 10>20
+
+// console.log(a>b)
+
+
+//undefined
+// let a;
+// let child = null;
+// child = "anish"
+
+// console.log(child)
+
+// operators
+
+// addition
+
+// let a = 30;
+// let b = 20;
+
+//    let c = a+b;
+//    console.log(c)
+
+
+// let c = a-b;
+// console.log(c)
+
+
+// let c = a*b;
+// console.log(c)
+
+
+// let c = a/b;
+// console.log(c)
+
+
+// let c = a%b;
+// console.log(c)
+
+
+// uniary operators
+
+// let a = 1
+// //  a = a+1;
+// a+=1; a= a+1;
+
+// a-=1; a = a-1
+ 
+
+// pre-increment opretor
+
+// let a = 10;
+
+//   let b = ++a;  //a+1  
+//  console.log(a);
+//  console.log(b);
+
+
+// post increment
+// let a = 10;
+
+//   let b = a++;  //a+1  
+//  console.log(a);
+//  console.log(b);
+
+// let age = 18;
+
+// console.log(age>18)
+
+
+
+// string
+
+// let name = 'a'
+// let number = 123;
+// let name1 = "hukum"
+// let num = 123
+// console.log(typeof(num))
+
+
+// let str = "mohali"; // indexing strarts from zero
+// let str1 = "kullu"
+// // console.log(str[4])
+// // console.log(str[8])
+// // console.log(str.length)
+
+// console.log(str +"  "+ str1)
+
+
+// template literals
+
+// let a = 10;
+// let b = 20;
+
+// console.log(`two number add ${a+b}`)
+
+
+
+
+// arithmetic operators
+
+// let a = 10;
+// let b = '10';
+
+// console.log(a==b) //only check value
+// console.log(a===b)  // value  and data type  both check
+
+
+// let a = 10;
+// let b = '10';
+
+// console.log(a!==b)
+// console.log(a!=b)
+
+
+// console.log(a!=b)
+
+// console.log(a!==b)
+// console.log(a!=b)
+
+
+
+// condition statment
+
+// 1.only if
+// 2. if-else
+// 3. else - if
+// 4. nested if-else
+
+
+// syntax only if
+
+// if(condition){
+//   //code
+// }
+
+
+// if(10>5){
+//     console.log("hello")
+// }
+
+
+// if(100>15){
+//   console.log("hello")
+//   console.log("world")
+// }
+
+// let age = 10;
+
+// if(age>18){
+//   console.log("you can vote")
+// }
+
+
+// if-else condition
+
+//  let age = 20;
+
+// if(age>18){
+//   console.log("you can vote")
+// } else{
+//   console.log("you cant vote")
+// }
+
+let age = 45;
+
+if(age<18){
+  console.log("you cant go outside")
+} else if(18<age<=25){
+  console.log("you can go outside")
+} else if (age>25){
+  console.log("you can go outside india")
+}else{
+  console.log("sorry")
+}
