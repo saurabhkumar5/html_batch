17a8b394a12ef7437bc6e591713d6793ab064910
saurabh kumar <saurabhkumar9493@gmail.com>
2023-12-29 21:41:19 +0530

    if-else

--- js/index.js
+++ js/index.js
@@ -198,14 +198,120 @@
 //   console.log("you cant vote")
 // }
 
-let age = 45;
 
-if(age<18){
-  console.log("you cant go outside")
-} else if(18<age<=25){
-  console.log("you can go outside")
-} else if (age>25){
-  console.log("you can go outside india")
-}else{
-  console.log("sorry")
-}
+
+//else -if
+// let age = 45;
+
+// if(age<18){
+//   console.log("you cant go outside")
+// } else if(18<age<=25){
+//   console.log("you can go outside")
+// } else if (age>25){
+//   console.log("you can go outside india")
+// }else{
+//   console.log("sorry")
+// }
+
+
+// 4. nested if-else
+
+// let a = 10;
+// let b = 20;
+// let c = 30
+
+// if(c>b){
+
+//   if(c<a){
+//        console.log("c is greater than a")
+//   }else{
+//      console.log("c is less than a")
+//   }
+
+// }else{
+//     console.log("hello ")
+// }
+
+
+
+
+
+// let a = 56;
+// let b = 77;
+// let c = 25;
+
+// if(c>b){
+
+//   if(a>b){
+//       console.log("komal") //hukum
+//   }
+  
+//   else{
+//      console.log("hukum")  //komal, gurprit,aakansha
+//   }
+
+// }
+// else{
+//      console.log("aakanskha")  
+// }
+
+
+
+// let a = 78;
+// let b = 200;
+// let c = 56;
+// let d = 250;
+
+// if(a>b){
+//   if(c>b){
+//     console.log("aankasha")  
+//   }
+//   else{
+//      console.log("gurpreet")
+//   }
+// }
+// else{
+//     if(d>c){
+//     console.log("komal") //hukum
+//     }
+//     else{
+//    console.log("hukum")   //komal, aaks
+//     }
+// }
+
+
+
+//logical operators
+
+// && logical AND operator
+// || logical OR operator
+// !  logical NOT operator
+
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// let d = 40;
+// // console.log(c>b && b>a)
+// // console.log(c>b && a>b)
+
+// console.log(d>c && c>b && a>b && d>a && c>a)
+
+
+// 2. or operator
+
+// let a = 10;
+// let b = 60;
+// let c = 42;
+// let d = 12;
+
+// console.log(c>b || a>c || d<b || c>b)
+
+
+// 3. not operator
+
+let a = 10;
+let b = 23;
+// let c = !(a>b);
+let c = !(b>a);
+console.log(c)
