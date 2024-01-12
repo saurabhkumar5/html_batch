e22e775edb7b530d3f537005eeca19def8318e08
saurabh kumar <saurabhkumar9493@gmail.com>
2024-01-12 13:00:41 +0530

    loop

--- js/loop.js
+++ js/loop.js
@@ -26,6 +26,44 @@
 
 
 
-for(let i = 1; i<=15; i=i+2){
-    console.log(i)
-}
+// for(let i = 1; i>0; i=i+2){
+//     console.log(i)
+// }
+
+// nested for loop
+
+// for(let i = 0; i<=3; i++){
+
+//     for(let j = 1; j<=3; j++){
+//         console.log("*");
+//     }
+// }
+
+
+// while loop
+
+// let i = 0;
+
+// while(i<=18){
+//    console.log(i)
+     
+//    i = i+2
+  
+// }
+
+
+// do-while loop
+
+
+let arr = ["mango","orange","apple","litchi"]
+
+    //   console.log(arr[0])
+    //   console.log(arr[1])
+    //   console.log(arr[2])
+    //   console.log(arr[3])
+
+    for(let i = 0; i<4; i++){
+        console.log(arr[1])
+    }
+
+
