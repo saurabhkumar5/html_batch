322a42795b60eccb9525c3b0913e078abdde4209
saurabh kumar <saurabhkumar9493@gmail.com>
2024-01-16 12:56:45 +0530

    function

---
+++ js/function.js
@@ -0,0 +1,48 @@
+
+
+// function Arshdeep(){
+//     console.log("aakanksha")
+// }
+// Arshdeep()
+// Arshdeep()
+// Arshdeep()
+// Arshdeep()
+
+
+// console.log(2+2)
+// console.log(2+5)
+
+// function Add(a,b,c){
+//     console.log(a*b+c)
+//     // console.log(a*b)
+// }
+
+// // Add(4,8)
+// // Add(7,10)
+// // Add(4,4)
+// // Add(56,89)
+
+// Add(4,3)
+// // Add()
+
+
+    // for(let i = 0; i<5; i++){
+    //     console.log(i)
+    // }
+
+    // for(let i = 0; i<5; i++){
+    //     console.log(i)
+    // }
+
+    // for(let i = 0; i<5; i++){
+    //     console.log(i)
+    // }
+
+function app(){
+    for(let i = 0; i<5; i++){
+        console.log(i)
+    }
+}
+
+app()
+app()
