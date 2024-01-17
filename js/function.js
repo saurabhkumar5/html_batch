d0b3c90f1f2c17bbe0b6063e9311974d8782189b
saurabh kumar <saurabhkumar9493@gmail.com>
2024-01-17 20:33:21 +0530

    function

--- js/function.js
+++ js/function.js
@@ -38,11 +38,91 @@
     //     console.log(i)
     // }
 
-function app(){
-    for(let i = 0; i<5; i++){
-        console.log(i)
-    }
-}
+// function app(){
+//     for(let i = 0; i<5; i++){
+//         console.log(i)
+//     }
+// }
 
-app()
-app()
+// app()
+// app()
+
+
+
+//  let value = function (){
+//     // console.log(8)
+
+//     return "saurabh"
+//     console.log("hukum")
+//     console.log(123)
+// }
+// // Apple();
+//  let a = value()
+//  console.log(a)
+
+// // if(a>0){
+// //     console.log("enjoy your life")
+// // }
+
+// // if(a>2){
+// //     console.log("happpy")
+// // }
+
+
+
+
+// let add = function(a,b){
+//     // let a = 10;
+//     // let b = 30;
+//     return a+b
+
+// }
+
+//  let a = add(10,20)
+//  console.log(a)
+
+
+
+
+
+1// simple function
+
+// function add(){
+
+// }
+// add()
+
+// 2.anoynmous funnction
+
+//  let value = function (){
+
+//   }
+//   value();
+
+// 3.arrow function
+
+    //   let arr  = ()=>{
+    //           console.log("gurpreet")
+    //      }
+    //      arr()
+
+
+//   let a =  ()=>console.log("vishu")
+//     a()
+
+            // let a  = ()=>console.log("apple");
+            // a()
+
+        //     let a = (a,b)=>{
+        //         return a+b
+        //     }
+        //   let b =  a(1,2)
+        //   console.log(b)
+
+        // let a = (a,b)=> a+b
+        //       let b =  a(1,2)
+        //       console.log(b)
+
+        
+
+            
