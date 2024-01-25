50c82659ac05a87029b993aba7f78c0cc9495678
saurabh kumar <saurabhkumar9493@gmail.com>
2024-01-25 21:00:06 +0530

    dom

---
+++ js/destructring.js
@@ -0,0 +1,52 @@
+// let d = "rajar"
+// let e = "vishu"
+// let f = "abc"
+
+// let x = ["rajat","vishu","abc"]
+
+
+// let arr = ["rajat","vishu","gurpreet","aarushi"]
+
+// // let a = arr[0]
+// // let b = arr[1];
+// // let c = arr[2]
+// // console.log(a,b,c)
+
+// let [a,b,c,d] = arr   let [value, setvalue] = useState()
+// console.log(d)
+
+
+// let obj = {
+//     name:"mannat",
+//     age:5,
+//     sub:"chemestry"
+// }
+
+// // let a = obj.name
+// // let b = obj.age
+// // console.log(a,b)
+
+// let {name,age,sub} = obj
+// console.log(name,age,sub)
+
+// setTimeout(()=>{
+//     console.log("es batch k students bahut lazy")
+// },2000)
+
+// setInterval(()=>{
+//     console.log("byeee")
+// },2000)
+
+
+
+// Math.obs(n)
+// Math.pow(a,b)
+// Math.floor(n)
+// Math.ceil(n)
+// Math.random()
+
+// console.log(Math.abs(-10))
+// console.log(Math.pow(2,4))
+// console.log(Math.floor(4.9))
+// console.log(Math.ceil(10.9))
+console.log(Math.floor(Math.random()*10))
